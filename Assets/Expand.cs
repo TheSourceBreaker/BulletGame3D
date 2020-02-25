1697597dd7f055845b35543d09be0eb018f5336e
Jahvier <jahvierhernandez11@gmail.com>
2020-02-24 21:30:47 -0800

    Update

---
+++ Assets/Expand.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Expand : MonoBehaviour
+{
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    private Vector3 scale;
+    private Vector3 tmp;
+    public float scaleSpeed;
+    public float desiredScale;
+    Transform rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scale = new Vector3(desiredScale, desiredScale, desiredScale);
+        rb = GetComponent<Transform>();
+        //timeBtwShots = startTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (timeBtwShots <= 0 && rb.transform.localScale != scale)
+        //{
+        //    rb.transform.localScale += scale * scaleSpeed;
+        //    timeBtwShots = startTimeBtwShots;
+        //}
+        //else
+        //    timeBtwShots -= Time.deltaTime;
+
+        tmp += scale * Time.deltaTime * scaleSpeed;
+    }
+}
